9fddc3e7b170c5e85d5c7db38843d0989bd06908
HIAndris <hegedus.i.andris@gmail.com>
2026-02-14 17:47:22 +0100

    edited welcome message

--- src/main/java/hiandris/radames/HelloController.java
+++ src/main/java/hiandris/radames/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Radames!");
+        welcomeText.setText("Welcome to Radames! <3");
     }
 }
